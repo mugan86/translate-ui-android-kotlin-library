a9135b85f7e91b9e70e9d6db0d2c8a2b8d3c7cfc
Anartz Mugika <mugan86@gmail.com>
2017-09-04 19:27:00 +0200

    Use kotlinx.android.synthetic options (kotlin-android-extensions) to use layout elements without declare with findViewById

--- app/src/main/java/amldev/translateapp/MainActivity.kt
+++ app/src/main/java/amldev/translateapp/MainActivity.kt
@@ -5,26 +5,22 @@ import android.content.Context
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
-import android.widget.Button
-import android.widget.TextView
-import org.jetbrains.anko.find
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    private var mTextMessage: TextView? = null
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                mTextMessage!!.setText(R.string.title_home)
+                message!!.setText(R.string.title_home)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                mTextMessage!!.setText(R.string.title_dashboard)
+                message!!.setText(R.string.title_dashboard)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                mTextMessage!!.setText(R.string.title_notifications)
+                message!!.setText(R.string.title_notifications)
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -40,46 +36,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val basqueButton: Button = find(R.id.basqueButton)
-        val englishButton: Button = find(R.id.englishButton)
-        val spanishButton: Button = find(R.id.spanishButton)
-
         LocaleHelper.setLocale(this, LocaleHelper.getLanguage(this))
 
         basqueButton.setOnClickListener {
             println("Change to basque" + LocaleHelper.getLanguage(this))
-            if (LocaleHelper.getLanguage(this) != "eu") changeLang("eu")
+            if (LocaleHelper.getLanguage(this) != "eu") LocaleHelper.changeLang(this, "eu")
         }
 
         englishButton.setOnClickListener {
             println("Change to english" + LocaleHelper.getLanguage(this))
-            if (LocaleHelper.getLanguage(this) != "en") changeLang("en")
+            if (LocaleHelper.getLanguage(this) != "en") LocaleHelper.changeLang(this, "en")
         }
 
         spanishButton.setOnClickListener {
             println("Change to spanish" + LocaleHelper.getLanguage(this))
-            if (LocaleHelper.getLanguage(this) != "es") changeLang("es")
+            if (LocaleHelper.getLanguage(this) != "es") LocaleHelper.changeLang(this, "es")
         }
 
-
-
-        mTextMessage = find(R.id.message)
-        val navigation: BottomNavigationView = find(R.id.navigation)
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
-
-
-
-    fun changeLang(lang: String) {
-        var changeLanguage: Boolean = false;
-        if (lang != LocaleHelper.getLanguage(this)) changeLanguage = true
-
-        if(changeLanguage) {
-            LocaleHelper.setLocale(this, lang)
-            println("change language from " + LocaleHelper.getLanguage(this) + " to " + lang)
-            LocaleHelper.restartApp(this)
-        }
-
-    }
-
 }
